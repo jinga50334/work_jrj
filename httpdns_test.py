fa994c5c13de6e2a1ad9642dc9cea1f61287c3be
hufu <abcd360@gmail.com>
2018-01-25 18:12:08 +0800

    httpdns change the judgement

--- httpdns_test.py
+++ httpdns_test.py
@@ -5,7 +5,7 @@ import requests
 def getip():
     hostinput = input('Please input the url:')
     hufur=requests.get("http://119.29.29.29/d",params={'dn': requests.urllib3.get_host(hostinput)[1]},timeout=0.1)
-    if hufur.headers.get('Content-Length') == str(0):
+    if len(hufur.content) == 0:
         print("The domain can not be resolved!")
     else:
         print("The real IP: %s" % (hufur.content.decode('utf-8')))
